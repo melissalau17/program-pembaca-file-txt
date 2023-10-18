0565fc024839584f32ff488ecbf9d62aaeece757
Melissa Lau <126906980+melissalau17@users.noreply.github.com>
2023-10-18 22:57:03 +0700

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("data.txt", "r"); // ganti "data.txt" dengan nama file yang sesuai
+
+    if (file == NULL) {
+        printf("File tidak dapat dibuka.\n");
+        return 1;
+    }
+
+    int id;
+    char firstName[50];
+    char lastName[50];
+    double amount;
+
+    printf("ID\tFirst Name\tLast Name\tAmount\n");
+    while (fscanf(file, "%d %s %s %lf", &id, firstName, lastName, &amount) != EOF) {
+        printf("%d\t%s\t%s\t%.2lf\n", id, firstName, lastName, amount);
+    }
+
+    fclose(file);
+
+    return 0;
+}
